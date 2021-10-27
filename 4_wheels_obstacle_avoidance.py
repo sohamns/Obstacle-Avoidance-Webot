a8b1c6a6f7d6731fb7d3e3517337eb5f684d2c8b
sohamns <66241369+sohamns@users.noreply.github.com>
2021-10-28 00:02:55 +0530

    Add files via upload

---
+++ 4_wheels_obstacle_avoidance.py
@@ -0,0 +1,57 @@
+"""4_wheels_collision_avoidance controller."""
+
+# You may need to import some classes of the controller module. Ex:
+#  from controller import Robot, Motor, DistanceSensor
+from controller import Robot, Motor
+
+# create the Robot instance.
+robot = Robot()
+
+# get the time step of the current world.
+# timestep = int(robot.getBasicTimeStep())
+TIME_STEP = 64
+
+# initialize sensors 
+ds = []
+dsNames = ['ds_right', 'ds_left']
+for i in range(2):
+    ds.append(robot.getDistanceSensor(dsNames[i]))
+    ds[i].enable(TIME_STEP)
+
+# You should insert a getDevice-like function in order to get the
+# instance of a device of the robot. Something like:
+#  motor = robot.getMotor('motorname')
+#  ds = robot.getDistanceSensor('dsname')
+#  ds.enable(timestep)
+
+# initialize motors
+wheels = []
+wheelsNames = ['wheel1', 'wheel2', 'wheel3', 'wheel4']
+for i in range(4):
+    wheels.append(robot.getMotor(wheelsNames[i]))
+    wheels[i].setPosition(float('inf'))
+    wheels[i].setVelocity(0.0)
+
+avoidObstacleCounter = 0
+
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+while robot.step(TIME_STEP) != -1:
+    leftSpeed = 1.0
+    rightSpeed = 1.0
+    if avoidObstacleCounter > 0:
+        avoidObstacleCounter -= 1
+        leftSpeed = 1.0
+        rightSpeed = -1.0
+    else:  # read sensors
+        for i in range(2):
+            if ds[i].getValue() < 950.0:
+                avoidObstacleCounter = 100
+    wheels[0].setVelocity(leftSpeed)
+    wheels[1].setVelocity(rightSpeed)
+    wheels[2].setVelocity(leftSpeed)
+    wheels[3].setVelocity(rightSpeed)
+    pass
+
+# Enter here exit cleanup code.
